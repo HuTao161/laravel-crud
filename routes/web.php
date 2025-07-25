ca7137509e38f7acb5cd1a26e73aaef2f2808048
Agung Ngurah <gungngurah161@gmail.com>
2025-07-25 09:26:52 +0800

    Menambahkan Routing untuk Produk

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdukController;
 
+// Route untuk halaman utama
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Resource route untuk ProdukController
+Route::resource('produk', ProdukController::class);
